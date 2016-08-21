13bf2c790c4c5798837d6a773e3550cca86ff653
wilsontechnicalconsulting <wilsontechnicalconsulting@gmail.com>
2016-08-21 15:57:53 -0700

    intermittent commit with Alamofire

---
+++ RainShine/WeatherVC.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherVC.swift
+//  RainShine
+//
+//  Created by Eric Wilson on 8/19/16.
+//  Copyright © 2016 Eric Wilson. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    @IBOutlet weak var dateLBL: UILabel!
+    @IBOutlet weak var currentTempLBL: UILabel!
+    @IBOutlet weak var locationLBL: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherCondition: UILabel!
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+    
+
+}
+
